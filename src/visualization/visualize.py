087485267c69acc148c2ef5608b534e8cb0b234d
trtrgfh <73056232+trtrgfh@users.noreply.github.com>
2023-05-29 11:21:10 -0400

    Update visualize.py

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
+from predict_model import *
+from make_dataset import *
 
 xgb.plot_tree(load_model, rankdir='LR')
 fig = plt.gcf()
 fig.set_size_inches(150, 100)
 plt.savefig('figure1.png', bbox_inches='tight')
 plt.show()
+
 im_features = load_model.feature_importances_
 plt.figure(figsize=(12,5))
 plt.bar(range(len(im_features)), im_features)
